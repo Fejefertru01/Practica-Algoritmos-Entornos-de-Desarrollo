cec822d6aad9499b980caef66ecd6ae0868f4aff
unknown <fejefertru@gmail.com>
2023-04-17 10:50:32 +0200

    Paquete para los test creado

---
+++ com.entornos.FernandoJesusFernandezTrujillo.arrays/src/com/entornos/FernandoJesusFernandezTrujillo/arrays/test/AllTests.java
@@ -0,0 +1,10 @@
+package com.entornos.FernandoJesusFernandezTrujillo.arrays.test;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ TestArrays.class })
+public class AllTests {
+
+}
